0c19f92b334590a39e55a58a01618cbe0998dfc5
Eric Gregoire <>
2026-06-15 11:47:38 -0400

    Colocate types to simplify things and tsconfig tweaks

--- src/cypress/cypress.ts
+++ src/cypress/cypress.ts
@@ -1,7 +1,37 @@
 import { E2EMailClient } from "../core/mailtm";
-import "./types";
+import type { PollingOptions, SearchFilters } from "../core/mailtm/types";
+import type { components } from "../core/api/types/mailtm-api";
 
 let mailClient: E2EMailClient | null = null;
+
+type Message = components["schemas"]["Message-message.read"];
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Initialize mailbox session
+       * @param address Email address
+       * @param password Account password
+       */
+      initializeMailbox(address: string, password: string): void;
+
+      /**
+       * Get most recent inbox match for an existing or new mail account
+       * @param filters Optional search filters
+       * @param options Configuration for message polling
+       */
+      searchMailbox(
+        filters?: SearchFilters,
+        options?: PollingOptions,
+      ): Chainable<Message>;
+
+      /**
+       * Delete the account mailbox and all its emails from the server
+       */
+      removeMailbox(): Chainable<void>;
+    }
+  }
+}
 
 Cypress.Commands.add("initializeMailbox", (address, password) => {
   cy.log(`Creating mailbox ${address}`);
--- src/playwright/playwright.ts
+++ src/playwright/playwright.ts
@@ -1,10 +1,31 @@
 import { test as base, expect, type TestType } from "@playwright/test"; // 1. Import TestType
 import { E2EMailClient } from "../core/mailtm";
-import type { MailFixtures } from "./types";
-
-export type { MailFixtures };
+import type { PollingOptions, SearchFilters } from "../core/mailtm/types";
+import type { components } from "../core/api/types/mailtm-api";
 
 let mailClient: E2EMailClient | undefined;
+
+type Message = components["schemas"]["Message-message.read"];
+
+export type MailFixtures = {
+  /**
+   * Initialize mailbox session
+   */
+  initializeMailbox: (address: string, password: string) => Promise<void>;
+
+  /**
+   * Get most recent inbox match for an existing or new mail account
+   */
+  searchMailbox: (
+    filters?: SearchFilters,
+    options?: PollingOptions,
+  ) => Promise<Message>;
+
+  /**
+   * Delete the account mailbox and all its emails from the server
+   */
+  removeMailbox: () => Promise<void>;
+};
 
 // 2. Explicitly type the "test" constant using TestType<MailFixtures>
 export const test: TestType<MailFixtures, {}> = base.extend<MailFixtures>({
